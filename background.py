e83199dc280d133536f5e02227fd45004c3f4c8f
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-08-17 08:20:29 +0530

    2020-08-17

--- background.py
+++ background.py
@@ -1,9 +1,11 @@
 import os
 import time
 from datetime import date
+from tqdm import trange
 
 while(1):
+	for _ in trange(1800):
+		time.sleep(1)
 	os.system('git add -A')
 	os.system('git commit -m "'+str(date.today())+'"')
 	os.system('git push')
-	time.sleep(1800)
